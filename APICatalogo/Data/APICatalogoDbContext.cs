b1a389d4dee7ac0f5710b244866118e7647946bd
Karolina Gomes <63608020+karolinagb@users.noreply.github.com>
2021-07-08 20:58:57 -0300

    Configurações iniciais realizadas
    Banco criado

---
+++ APICatalogo/Data/APICatalogoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Data
+{
+    public class APICatalogoDbContext : DbContext
+    {
+        public APICatalogoDbContext(DbContextOptions<APICatalogoDbContext> options) : base(options)
+        {
+               
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.Models
+{
+    public class Categoria
+    {
+        [Key] //Atributo para chave primária
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(80)]
+        public string Nome { get; set; }
+        [Required]
+        [MaxLength(300)]
+        public string ImagemUrl { get; set; }
+        public ICollection<Produto> Produtos { get; set; }
+    }
+}
