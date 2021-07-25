95f9d0de986d7c61f1215b0b0c751be39c541e6e
Karolina Gomes <63608020+karolinagb@users.noreply.github.com>
2021-07-25 14:36:28 -0300

    Erro [PENDENTE]
    Comitando para mostrar ao professor do curso

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,70 @@
+﻿using APICatalogo.Data;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Routing;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace APICatalogo.Controllers
+{
+    [Route("api/[Controller]")] //Para definir a rota de acesso a API. Sem esse atributo nao tem como acessar as APIs. Ele faz um processo de mapeamento das
+    //requisições recebidas para a lógica dos métodos actions do controlador. Mapeia para o metodo action correspondente.
+    [ApiController] //Habilita mais recursos que facilitam o desenvolvimento
+    public class ProdutosController : ControllerBase //Herda de controller base para ter recursos de atender requisições HTTP
+                                                     //A classe ControllerBase vai incluir diversas funcionalidade para uma API e vai omitir as funcionalidades de suporte as Views
+    {
+        private readonly APICatalogoDbContext _aPICatalogoDbContext;
+
+        public ProdutosController(APICatalogoDbContext aPICatalogoDbContext)
+        {
+            _aPICatalogoDbContext = aPICatalogoDbContext;
+        }
+
+
+        [HttpGet]
+        //Retorna todos os produtos
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            //Entity Framework rastreia a consulta para ver se teve alteração no objeto
+            //Isso pode deixar o desempenho ruim, então em ações Get nós debilitamos isso através do
+            //AsNoTracking
+            return _aPICatalogoDbContext.Produtos.AsNoTracking().ToList();
+        }
+
+        [HttpGet("{id}"), Name = "ObterProduto"]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _aPICatalogoDbContext.Produtos.AsNoTracking().FirstOrDefault(x => x.Id == id);
+            if(produto == null)
+            {
+                return NotFound(); //404
+            }
+            return produto;
+        }
+
+        //FromBody = para Asp.Net Core saber que ela tem que obter essas informações de produto
+        //do corpo da requisição
+        //O ModelBind vincula os parÂmetros do corpo do request com os parâmetros da action post
+        [HttpPost]
+        public ActionResult Post([FromBody]Produto produto)
+        {
+            //O ModelState é uma propriedade da classe controller que representa uma coleção de pares "nome" "valor"
+            //que são submetidos no servidor durante o post. Ele cnontém uma cleção de mensagens de erro para cada valor submetido.
+            //Essa propriedade não tem conhecimento do nosso modelo. Ela só tem nomes, valores e erros. Ela serve para armazenar o valor submetido ao servidor
+            //e para armazenar os erros.
+            //OBS: Desde a versão 2.0 do .NET, para controladores com atributo´[ApiController], essa validação é feita automaticamente.
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+            _aPICatalogoDbContext.Produtos.Add(produto);
+            _aPICatalogoDbContext.SaveChanges();
+
+            //Retornar o codigo 201 CreatedAt
+            //Quando criamos um produto, temos que retornar a localização de um produto
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.Id}, produto);
+            //Acima estou passando id como parametro para a action get e o produto que virá no corpo
+        }
+    }
+}
---
+++ APICatalogo/Migrations/20210712185600_PopuladoOBancoDeDados.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopuladoOBancoDeDados : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Categorias(Nome, ImagemUrl) VALUES ('Bebidas', 'http://www.macoratti.net/Imagens/1.jpg')");
+            mb.Sql("INSERT INTO Categorias(Nome, ImagemUrl) VALUES ('Lanches', 'http://www.macoratti.net/Imagens/2.jpg')");
+            mb.Sql("INSERT INTO Categorias(Nome, ImagemUrl) VALUES ('Sobremesas', 'http://www.macoratti.net/Imagens/3.jpg')");
+
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+                                            "Values('Coca-Cola Diet','Refrigerante de Cola 350 ml',5.45,'http://www.macoratti.net/Imagens/coca.jpg',50,now(),(Select Id from Categorias where Nome='Bebidas'))");
+
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+                                "Values('Lanche de Atum','Lanche de Atum com maionese',8.50,'http://www.macoratti.net/Imagens/atum.jpg',10,now(),(Select Id from Categorias where Nome='Lanches'))");
+
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+                                "Values('Pudim 100 g','Pudim de leite condensado 100g',6.75,'http://www.macoratti.net/Imagens/pudim.jpg',20,now(),(Select Id from Categorias where Nome='Sobremesas'))");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categorias");
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
